54ca65edddbb1e96741c6db537384a04d03f2cfc
Shirley Alus <shirleyal@mta.ac.il>
2021-12-17 16:50:29 +0200

    initial

--- global_users.js
+++ global_users.js
@@ -1,6 +1,9 @@
 const {create_json_file} = require('./json_file_handling')
+const UsersList = require('./users/users_list')
+const json_users = 'users.json'
 
 const g_users = [ {id:1, name: 'Root'} ];
-create_json_file(g_users)
+create_json_file(g_users, json_users)
+users = new UsersList()
 
 module.exports = g_users
