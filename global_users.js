d03538032780a67c3b3051c221c204608121c45b
Shirley Alus <shirleyal@mta.ac.il>
2021-12-17 21:40:28 +0200

    minur fixes

--- global_users.js
+++ global_users.js
@@ -1,11 +1,9 @@
 const {create_json_file} = require('./json_file_handling')
 const UsersList = require('./users/users_list')
-const fs = require('fs');
 
 const json_users = 'users.json'
 
-const g_users = [ {id:1, name: 'Root'} ];
 dict = create_json_file(json_users)
 const users_list = new UsersList(dict)
 
-module.exports = {g_users, users_list }
+module.exports = {users_list,json_users }
